1676a54e02ffcd10437b85410e2517c8e0877e4e
Ivan <guryanov333@gmail.com>
2021-11-18 00:24:40 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let btnClear = document.querySelector('.button__clear');
+let total = document.querySelector('.value');
+let equality = document.querySelector('.equality');
+
+let list = document.getElementsByTagName('span');
+
+for (let i = 0; i < list.length; i++) {
+	list[i].addEventListener('click', function (e) {
+		total.innerHTML += this.innerHTML;
+	}); /*при нажатии на кнопку её значение записывается в total*/
+
+}
+
+btnClear.addEventListener('click', function (e) {
+	total.innerHTML = '';
+});
+
+equality.addEventListener('click', function (e) {
+	total.innerHTML = eval(total.innerHTML);
+});
